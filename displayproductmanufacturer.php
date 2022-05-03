82214d2edf2a09320cd560d06da9e8ca6e869418
Matt75 <5262628+Matt75@users.noreply.github.com>
2022-05-03 19:58:12 +0200

    Adds support of new Product Grid on PrestaShop 8.0.0

--- displayproductmanufacturer.php
+++ displayproductmanufacturer.php
@@ -24,15 +24,6 @@ if (!defined('_PS_VERSION_')) {
 class DisplayProductManufacturer extends Module
 {
     /**
-     * @var array list of hooks used
-     */
-    public $hooks = [
-        'actionAdminControllerSetMedia',
-        'actionAdminProductsListingFieldsModifier',
-        'actionAdminProductsListingResultsModifier',
-    ];
-
-    /**
      * Name of ModuleAdminController used for configuration
      */
     const MODULE_ADMIN_CONTROLLER = 'AdminDisplayProductManufacturer';
@@ -48,13 +39,18 @@ class DisplayProductManufacturer extends Module
     private $isPrestaShop16;
 
     /**
+     * @var bool
+     */
+    private $isPrestaShop8;
+
+    /**
      * Constructor.
      */
     public function __construct()
     {
         $this->name = 'displayproductmanufacturer';
         $this->tab = 'administration';
-        $this->version = '3.0.0';
+        $this->version = '4.0.0';
         $this->author = 'Matt75';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = [
@@ -62,6 +58,7 @@ class DisplayProductManufacturer extends Module
             'max' => '1.7.9.99',
         ];
         $this->isPrestaShop16 = (bool) version_compare(_PS_VERSION_, '1.7.0.0', '<=');
+        $this->isPrestaShop8 = (bool) version_compare(_PS_VERSION_, '8.0.0', '>=');
 
         parent::__construct();
 
@@ -77,9 +74,28 @@ class DisplayProductManufacturer extends Module
     public function install()
     {
         return parent::install()
-            && $this->registerHook($this->hooks)
+            && $this->installHooks()
             && $this->installTabs()
             && Configuration::updateValue(static::CONFIGURATION_KEY_SHOW_LOGO, false);
+    }
+
+    public function installHooks()
+    {
+        $hooks = [];
+
+        if (false === $this->isPrestaShop8) {
+            $hooks[] = 'actionAdminControllerSetMedia';
+            $hooks[] = 'actionAdminProductsListingFieldsModifier';
+            $hooks[] = 'actionAdminProductsListingResultsModifier';
+        }
+
+        if ($this->isPrestaShop8 || version_compare(_PS_VERSION_, '1.7.8.0', '>=')) {
+            $hooks[] = 'actionProductGridDefinitionModifier';
+            $hooks[] = 'actionProductGridDataModifier';
+            $hooks[] = 'actionProductGridQueryBuilderModifier';
+        }
+
+        return $this->registerHook($hooks);
     }
 
     /**
@@ -144,7 +160,8 @@ class DisplayProductManufacturer extends Module
      */
     public function hookActionAdminControllerSetMedia()
     {
-        if ('AdminProducts' === Tools::getValue('controller')
+        if (!$this->isPrestaShop8
+            && 'AdminProducts' === Tools::getValue('controller')
             && false === Tools::getIsset('addproduct')
             && Configuration::get(static::CONFIGURATION_KEY_SHOW_LOGO)
         ) {
@@ -157,7 +174,7 @@ class DisplayProductManufacturer extends Module
      *
      * @param array $params
      */
-    public function hookActionAdminProductsListingFieldsModifier(array $params)
+    public function hookActionAdminProductsListingFieldsModifier(array &$params)
     {
         if ($this->isPrestaShop16) {
             // If hook is called in AdminController::processFilter() we have to check existence
@@ -215,7 +232,7 @@ class DisplayProductManufacturer extends Module
      *
      * @param array $params
      */
-    public function hookActionAdminProductsListingResultsModifier(array $params)
+    public function hookActionAdminProductsListingResultsModifier(array &$params)
     {
         if (!Configuration::get(static::CONFIGURATION_KEY_SHOW_LOGO)) {
             return;
@@ -235,6 +252,126 @@ class DisplayProductManufacturer extends Module
                 if ($product['id_manufacturer']) {
                     $params['products'][$key]['manufacturer_image'] = $this->context->link->getMediaLink(_THEME_MANU_DIR_ . $product['id_manufacturer'] . '.jpg');
                 }
+            }
+        }
+    }
+
+    /**
+     * Hook allows to modify product grid definition since 8.0.0
+     *
+     * @param array $params
+     */
+    public function hookActionProductGridDefinitionModifier(array $params)
+    {
+        if (empty($params['definition'])) {
+            return;
+        }
+
+        /** @var PrestaShop\PrestaShop\Core\Grid\Definition\GridDefinitionInterface $definition */
+        $definition = $params['definition'];
+
+        if (Configuration::get(static::CONFIGURATION_KEY_SHOW_LOGO)) {
+            $column = new PrestaShop\PrestaShop\Core\Grid\Column\Type\Common\ImageColumn('manufacturer_name');
+            $column->setName($this->l('Manufacturer'));
+            $column->setOptions([
+                'src_field' => 'manufacturer_logo',
+                'clickable' => false,
+            ]);
+        } else {
+            $column = new PrestaShop\PrestaShop\Core\Grid\Column\Type\DataColumn('manufacturer_name');
+            $column->setName($this->l('Manufacturer'));
+            $column->setOptions([
+                'field' => 'manufacturer_name',
+            ]);
+        }
+
+        $definition
+            ->getColumns()
+            ->addAfter(
+                'category',
+                $column
+            )
+        ;
+
+        /** @var PrestaShop\PrestaShop\Adapter\Form\ChoiceProvider\ManufacturerNameByIdChoiceProvider $manufacturerNameByIdChoiceProvider */
+        $manufacturerNameByIdChoiceProvider = $this->get('prestashop.adapter.form.choice_provider.manufacturer_name_by_id');
+
+        $definition->getFilters()->add(
+            (new PrestaShop\PrestaShop\Core\Grid\Filter\Filter('manufacturer_name', Symfony\Component\Form\Extension\Core\Type\ChoiceType::class))
+                ->setAssociatedColumn('manufacturer_name')
+                ->setTypeOptions([
+                    'required' => false,
+                    'choices' => $manufacturerNameByIdChoiceProvider->getChoices(),
+                    'translation_domain' => false,
+                ])
+        );
+    }
+
+    /**
+     * Hook allows to modify experimental product grid data since 8.0.0
+     *
+     * @param array $params
+     */
+    public function hookActionProductGridDataModifier(array &$params)
+    {
+        if (empty($params['data']) || !Configuration::get(static::CONFIGURATION_KEY_SHOW_LOGO)) {
+            return;
+        }
+
+        /** @var PrestaShop\PrestaShop\Core\Grid\Data\GridData $gridData */
+        $gridData = $params['data'];
+        $modifiedRecords = $gridData->getRecords()->all();
+        /** @var PrestaShop\PrestaShop\Adapter\Manufacturer\ManufacturerLogoThumbnailProvider $manufacturerLogoThumbnailProvider */
+        $manufacturerLogoThumbnailProvider = $this->get('prestashop.adapter.manufacturer.manufacturer_logo_thumbnail_provider');
+
+        foreach ($modifiedRecords as $key => $data) {
+            if (isset($data['id_manufacturer'])) {
+                $modifiedRecords[$key]['manufacturer_logo'] = $manufacturerLogoThumbnailProvider->getPath($data['id_manufacturer']);
+            }
+        }
+
+        $params['data'] = new PrestaShop\PrestaShop\Core\Grid\Data\GridData(
+            new PrestaShop\PrestaShop\Core\Grid\Record\RecordCollection($modifiedRecords),
+            $gridData->getRecordsTotal(),
+            $gridData->getQuery()
+        );
+    }
+
+    /**
+     * Hook allows to modify experimental product grid query builder and add custom sql statements since 8.0.0
+     *
+     * @param array $params
+     */
+    public function hookActionProductGridQueryBuilderModifier(array $params)
+    {
+        if (empty($params['search_query_builder']) || empty($params['search_criteria'])) {
+            return;
+        }
+
+        /** @var Doctrine\DBAL\Query\QueryBuilder $searchQueryBuilder */
+        $searchQueryBuilder = $params['search_query_builder'];
+
+        /** @var PrestaShop\PrestaShop\Core\Search\Filters\ProductFilters $searchCriteria */
+        $searchCriteria = $params['search_criteria'];
+
+        $searchQueryBuilder->addSelect(
+            'p.`id_manufacturer`, man.`name` AS `manufacturer_name`'
+        );
+
+        $searchQueryBuilder->leftJoin(
+            'p',
+            '`' . _DB_PREFIX_ . 'manufacturer`',
+            'man',
+            'man.`id_manufacturer` = p.`id_manufacturer`'
+        );
+
+        if ('manufacturer_name' === $searchCriteria->getOrderBy()) {
+            $searchQueryBuilder->orderBy('man.`manufacturer_name`', $searchCriteria->getOrderWay());
+        }
+
+        foreach ($searchCriteria->getFilters() as $filterName => $filterValue) {
+            if ('manufacturer_name' === $filterName) {
+                $searchQueryBuilder->setParameter('manufacturer_name', $filterValue);
             }
         }
     }
